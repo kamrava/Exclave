c791d249cb983da8e16e8fdb9faf59e232b9fb8a
Hamed Kamrava <h.kamrava1@gmail.com>
2024-10-19 15:29:40 +0100

    cleanup

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,4 +15,5 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.51.0")
+    implementation("com.google.gms:google-services:4.4.2")
 }
