bca03ca493e7f4f5d4df964733b60309a2c492f6
Hamed Kamrava <h.kamrava1@gmail.com>
2024-10-19 15:21:50 +0100

    add events to VpnService

--- app/src/main/java/io/nekohasekai/sagernet/vpn/services/VpnService.kt
+++ app/src/main/java/io/nekohasekai/sagernet/vpn/services/VpnService.kt
@@ -2,32 +2,48 @@ package io.nekohasekai.sagernet.vpn.services
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import io.nekohasekai.sagernet.R
 import io.nekohasekai.sagernet.SagerNet
 import io.nekohasekai.sagernet.database.DataStore
 import io.nekohasekai.sagernet.ui.VpnRequestActivity
-import io.nekohasekai.sagernet.vpn.repositories.AppRepository
+import io.nekohasekai.sagernet.vpn.interfaces.VpnEventListener
 
 @SuppressLint("StaticFieldLeak")
 object VpnService {
     private lateinit var context: Context
     private lateinit var connect: ActivityResultLauncher<Void?>
-
+    private val listeners = mutableListOf<VpnEventListener>()
+    var canStop: Boolean = true
 
     fun initialize(activity: AppCompatActivity) {
         this.context = activity.applicationContext
 
         connect = activity.registerForActivityResult(VpnRequestActivity.StartService()) {
             if (it) {
-                println("HAMED_LOG_" + R.string.vpn_permission_denied)
+                Toast.makeText(activity, R.string.vpn_permission_denied, Toast.LENGTH_LONG).show()
             }
         }
     }
 
     fun startVpn() {
-        if (AppRepository.canStop) SagerNet.stopService() else connect.launch(null)
+        if (!SagerNet.started) {
+            connect.launch(null)
+            listeners.forEach { it.onVpnStarted() }
+        }
+    }
+
+    fun stopVpn() {
+        if (canStop) {
+            SagerNet.stopService()
+            listeners.forEach { it.onVpnStopped() }
+        }
+    }
+
+    fun toggleVpn() {
+        if (isStarted()) stopVpn() else startVpn()
     }
 
     fun startOrReloadVpn() {
@@ -38,8 +54,23 @@ object VpnService {
         startVpn()
     }
 
+    fun isStarted(): Boolean {
+        return SagerNet.started
+    }
+
     fun startVpnFromProfile(profileId: Long) {
         DataStore.selectedProxy = profileId
         startOrReloadVpn()
+        listeners.forEach { it.onVpnServerChanged(profileId) }
+    }
+
+    // Function to add listeners
+    fun addVpnEventListener(listener: VpnEventListener) {
+        listeners.add(listener)
+    }
+
+    // Function to remove listeners
+    fun removeVpnEventListener(listener: VpnEventListener) {
+        listeners.remove(listener)
     }
 }
